63d13e34d7c1d6ea6b483f473292a7a84cd0c736
Osmar Villalba <osmarvillalba@Osmars-MacBook-Air.local>
2024-02-27 16:32:27 -0300

    FIX: change paciente column name change status name to ativo

---
+++ src/main/java/med/voll/api/entity/Paciente.java
@@ -0,0 +1,12 @@
+package med.voll.api.entity;
+
+
+public class Paciente {
+    private Long id;
+    private String nome;
+    private String email;
+    private String telefone;
+    private String cpf;
+    private Address endereco;
+    private boolean ativo;
+}
---
+++ src/main/resources/db/migration/V6__alter-table-pacientes-add-column-ativo.sql
@@ -0,0 +1,1 @@
+ALTER TABLE pacientes ADD ativo tinyint DEFAULT 1 NOT NULL;
