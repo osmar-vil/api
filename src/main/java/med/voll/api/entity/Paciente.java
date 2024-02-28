f03827677ecf2145b3b3307af1d8c1bdef0a3850
Osmar Villalba <osmarvillalba@Osmars-MacBook-Air.local>
2024-02-28 10:45:16 -0300

    FEAT: added paciente entity

--- src/main/java/med/voll/api/entity/Paciente.java
+++ src/main/java/med/voll/api/entity/Paciente.java
@@ -1,12 +1,26 @@
 package med.voll.api.entity;
 
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table(name = "medicos")
+@Entity(name = "Medico")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
 public class Paciente {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
     private String email;
     private String telefone;
     private String cpf;
+    @Embedded
     private Address endereco;
     private boolean ativo;
 }
