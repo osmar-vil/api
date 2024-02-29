b343ec5eb3c31f201b1ee0956ad722e260b34e5d
Osmar Villalba <osmarvillalba@Osmars-MacBook-Air.local>
2024-02-29 14:53:51 -0300

    FEAT: included entity to users

---
+++ src/main/java/med/voll/api/entity/User.java
@@ -0,0 +1,20 @@
+package med.voll.api.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table(name = "users")
+@Entity(name = "User")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class User {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String login;
+    private String password;
+}
