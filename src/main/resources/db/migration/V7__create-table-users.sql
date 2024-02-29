980a8e3822d511a8e99061ead023812d845cf87f
Osmar Villalba <osmarvillalba@Osmars-MacBook-Air.local>
2024-02-29 15:05:32 -0300

    FEAT: included migration to users table

---
+++ src/main/resources/db/migration/V7__create-table-users.sql
@@ -0,0 +1,10 @@
+CREATE TABLE users (
+
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    login VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    is_active TINYINT DEFAULT 1 NOT NULL,
+
+    PRIMARY KEY (id)
+
+);
