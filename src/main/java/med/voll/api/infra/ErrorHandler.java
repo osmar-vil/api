9fe9d625d6d304fc3bb0af3b44c19190b25bc250
Osmar Villalba <osmarvillalba@Osmars-MacBook-Air.local>
2024-02-29 09:02:19 -0300

    FEAT: include bad request error handler

--- src/main/java/med/voll/api/infra/ErrorHandler.java
+++ src/main/java/med/voll/api/infra/ErrorHandler.java
@@ -2,6 +2,8 @@ package med.voll.api.infra;
 
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -11,6 +13,18 @@ public class ErrorHandler {
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity<?> notFoundException() {
         return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> badRequest(MethodArgumentNotValidException ex) {
+        var errors = ex.getFieldErrors();
+        return ResponseEntity.badRequest().body(errors.stream().map(ValidationErrors::new ).toList());
+    }
+
+    private record ValidationErrors( String campo, String menssagem ) {
+        public ValidationErrors (FieldError error) {
+            this(error.getField(), error.getDefaultMessage());
+        }
     }
 
 }
