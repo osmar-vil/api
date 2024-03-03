c9e8a6a2bb86dfc04b70253efc308c9e85495867
Osmar Villalba <osmarvillalba@Osmars-MacBook-Air.local>
2024-03-03 18:38:54 -0300

    FEAT: generated token

--- src/main/java/med/voll/api/controller/AuthController.java
+++ src/main/java/med/voll/api/controller/AuthController.java
@@ -2,7 +2,10 @@ package med.voll.api.controller;
 
 import jakarta.validation.Valid;
 import med.voll.api.dto.auth.LoginRequest;
+import med.voll.api.entity.User;
+import med.voll.api.infra.security.TokenService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,10 +20,15 @@ public class AuthController {
     @Autowired
     private AuthenticationManager manager;
 
+    @Autowired
+    private TokenService tokenService;
+
     @PostMapping
-    public void login(@RequestBody @Valid LoginRequest request) {
+    public ResponseEntity<?> login(@RequestBody @Valid LoginRequest request) {
         var token = new UsernamePasswordAuthenticationToken(request.login(), request.password());
         var authentication = manager.authenticate(token);
+
+        return ResponseEntity.ok(tokenService.generateToken( (User) authentication.getPrincipal()));
     }
 
 }
