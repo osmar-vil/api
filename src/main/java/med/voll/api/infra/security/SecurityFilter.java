5ea690a1b69646abdb95600ae1191100d0fca13e
Osmar Villalba <osmarvillalba@Osmars-MacBook-Air.local>
2024-03-05 09:26:08 -0300

    FEAT: added token security filter to validate if the user sent into the header authorization the token string

---
+++ src/main/java/med/voll/api/infra/security/SecurityFilter.java
@@ -0,0 +1,27 @@
+package med.voll.api.infra.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        var tokenJwt = getToken(request);
+        System.out.println(tokenJwt);
+        filterChain.doFilter(request, response);
+    }
+
+    private String getToken(HttpServletRequest request) {
+        var authorizationHeader = request.getHeader("Authorization");
+        if (authorizationHeader == null) throw new RuntimeException("Invalid token or missing it, please provided a valid one");
+        return authorizationHeader.replace("Bearer ", "");
+    }
+}
