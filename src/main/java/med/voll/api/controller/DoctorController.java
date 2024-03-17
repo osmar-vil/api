c42f6e732489b11f1efb9bf9db72490ffa47d9a9
Osmar Villalba <osmarvillalba@Osmars-MacBook-Air.local>
2024-03-17 17:25:38 -0300

    FEAT: included security schemas into swagger

--- src/main/java/med/voll/api/controller/DoctorController.java
+++ src/main/java/med/voll/api/controller/DoctorController.java
@@ -1,5 +1,6 @@
 package med.voll.api.controller;
 
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
 import med.voll.api.domain.medico.DoctorCreateRequest;
 import med.voll.api.domain.medico.DoctorResponse;
@@ -18,6 +19,7 @@ import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 @RequestMapping("/doctors")
+@SecurityRequirement(name = "bearer-key")
 public class DoctorController {
 
     @Autowired
